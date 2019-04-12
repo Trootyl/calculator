f11bd062c3806fab5b958fa3ca4063bc97fca57b
Trootyl <49168857+Trootyl@users.noreply.github.com>
2019-04-12 16:42:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
+/* kalkulator.c -- po prostu kalkulator
+autor - Pawel Stateczny
+ostatnia aktualizacja - 12.04.2019 / 16:41 */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
     long double liczba1, liczba2, wynik;
     char wybor;
@@ -10,37 +14,36 @@ int main()
     printf("=============================================================");
     do
     {
-        printf("\n\nJesli chcesz wyjsc z programu wpisz 007\n\nPodaj pierwsza liczbe: ");
+        printf("\n\nJesli chcesz wyjsc z programu wpisz -777\n\nPodaj pierwsza liczbe: ");
         scanf("%Lf", &liczba1);
-        if(liczba1 != 007)
+        if(liczba1 != -777)
         {
-        printf("Wybierz dzialanie: +  -  *  /       ");
-        scanf(" %c", &wybor);
+            printf("Wybierz dzialanie: +  -  *  /       ");
+            scanf(" %c", &wybor);
             if(wybor != '+' && wybor != '-' && wybor != '*' && wybor != '/')
             {
-            printf("Podales niepoprawny typ dzialania, wprowadz jeszcze raz     ");
-            scanf(" %c", &wybor);
+                printf("Podales niepoprawny typ dzialania, wprowadz jeszcze raz.     ");
+                scanf(" %c", &wybor);
             }
-        printf("Podaj druga liczbe: ");
-        scanf("%Lf", &liczba2);
+            printf("Podaj druga liczbe: ");
+            scanf("%Lf", &liczba2);
             switch(wybor)
             {
-            case '+': wynik = liczba1 + liczba2;
+                case '+': wynik = liczba1 + liczba2;
                 break;
-            case '-': wynik = liczba1 - liczba2;
+                case '-': wynik = liczba1 - liczba2;
                 break;
-            case '*': wynik = liczba1 * liczba2;
+                case '*': wynik = liczba1 * liczba2;
                 break;
-            case '/': wynik = liczba1 / liczba2;
+                case '/': wynik = liczba1 / liczba2;
                 break;
-            default: printf("Podales niepoprawny typ dzialania\n");
+                default: printf("Podales niepoprawny typ dzialania.\n");
             }
-        printf("Wynik: %Lf %c %Lf = %Lf\n\n",liczba1, wybor, liczba2, wynik);
-        printf("=============================================================");
+            printf("Wynik: %Lf %c %Lf = %Lf\n\n",liczba1, wybor, liczba2, wynik);
+            printf("=============================================================");
         }
         else
             return 0;
     }  while(1);
-return 0;
+    return 0;
 }
-
